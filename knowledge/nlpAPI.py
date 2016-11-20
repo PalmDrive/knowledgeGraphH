b90d10fa874a69796d1b681af155bac85c26cf52
Yong <yonglin88@gmail.com>
2016-11-20 14:53:57 -0800

    nlp working

---
+++ knowledge/nlpAPI.py
@@ -0,0 +1,42 @@
+# coding=utf8
+
+import urllib
+import argparse
+import json
+
+API_KEY = 'K8L7C0w9yxVGAK3Q7q3euHtsbtfzOgMsya4Fyb8x'
+
+class nlpAPI(object):
+
+    def call(self, text, pattern):
+        return urllib.urlopen(self.get_url(text, pattern)).read()
+
+    def get_url(self, text, pattern):
+        params = {
+            "api_key": API_KEY,
+            "text": text,
+            "pattern": pattern,
+            "format": "json",
+        }
+        url = 'http://api.ltp-cloud.com/analysis/?' + urllib.urlencode(params)
+        return url
+
+def handle_request(response):
+    body = json.loads(response)
+    for paragraph in body:
+        for sentence in paragraph:
+            for word in sentence:
+                print word
+                print word['id']
+                print word['cont']
+#
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'text', help='text')
+    parser.add_argument(
+        'pattern', help='pattern = dp, sdp, srl or all')
+    args = parser.parse_args()
+    api = nlpAPI()
+    handle_request(api.call(args.text, args.pattern))
