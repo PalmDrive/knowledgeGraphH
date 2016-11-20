d1bc4390aa6f3aad8e14c667c3b9c34f9010ba57
Junchao Wu <junchaow@uber.com>
2016-11-20 12:36:01 -0800

    project setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages
+from setuptools import setup
+
+version = '0.0.1'
+
+setup(
+    name='knowledge graph',
+    version=version,
+    description="hackathon",
+    long_description="""\
+    """,
+    classifiers=[],
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='',
+    author_email='',
+    url='',
+    license='',
+    packages=find_packages(
+        exclude=['ez_setup', 'examples', 'tests', 'tests.*']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+)
