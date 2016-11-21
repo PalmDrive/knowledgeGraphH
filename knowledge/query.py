7eef0d4a3ce035f5038407b4d0d1d76badcb200a
Yong <yonglin88@gmail.com>
2016-11-20 20:36:12 -0800

    tested

--- knowledge/query.py
+++ knowledge/query.py
@@ -1,26 +1,26 @@
 # coding=utf8
 
 import argparse
-import ltp
+from ltp import nlpAPI
 import json
-from neo4j.v1 import GraphDatabase
 
 class Db(object):
-    def find_entity(self, entity_name):
-        pass
+    def find_from_entity(self, edge):
+        return 'from_entity <- ' + str(edge)
 
-    def find_entity(self, from_entity, edge):
-        pass
+    def find_to_entity(self, edge):
+        return 'to_entity <- ' + str(edge)
 
-    def find_relation(self, relation_name):
-        pass
+    def find_relations(self, relation_name, **kwargs):
+        edge = {'relation' : 'relation_name ' + relation_name + str(kwargs.items()), 'IO':'abc'}
+        print edge
+        return [edge]
 
-    def find_relation(self, from_entity, to = None):
-        pass
 
 class Word(object):
     def __init__(self, dict):
         self.word = dict
+        self.parent = None
 
     def is_subject(self):
         return self.word['relate'] == 'SBV'
@@ -29,56 +29,102 @@ class Word(object):
         return self.word['relate'] == 'HED'
 
     def is_direct_object(self):
-        return self.word['relate'] in ['VOB', 'FOB']
+        # print self.word
+        # print 'parent : ' + str(self.parent_id())
+        # print str(self.parent.id())
+        return self.word['relate'] in ['VOB', 'FOB'] and self.parent and self.parent.is_relation()
 
     def is_indirect_object(self):
-        return self.word['relate'] == 'IOB'
+        return self.word['relate'] == 'IOB' \
+               or \
+                (
+                   (self.word['relate'] == 'VOB' or self.word['relate'] == 'POB')
+                    and self.parent
+                    and self.parent.get_parent()
+                    and self.parent.get_parent().is_relation()
+                )
+
+    def is_complement(self):
+        return self.word['relate'] == 'CMP'
 
     def is_unknown_word(self):
-        return self.word['cont'] in ['谁','什么','怎么','怎么样','为什么']
+        return self.word['cont'] in [u'谁',u'什么',u'怎么',u'怎么样',u'为什么',u'哪个']
 
     def get_content(self):
         return self.word['cont']
 
+    def id(self):
+        return self.word['id']
+
+    def parent_id(self):
+        return self.word['parent']
+
+    def set_parent(self, parent):
+        self.parent = parent
+
+    def get_parent(self):
+        return self.parent
+
 
 class Query(object):
     def lookup(self, text):
-        api = ltp()
-        self.analyze_query(api.call(text, 'all'))
-        pass
+        api = nlpAPI()
+        res = api.call(text, 'all')
+        body = json.loads(res)
+        return ', '.join(self.query(body))
 
-    def analyze_query(self, response):
-        body = json.loads(response)
-
+    def query(self, body):
         subj = None
         verb = None
         direct_obj = None
         indirect_obj = None
 
+        words = {}
         for paragraph in body:
             for sentence in paragraph:
                 for word in sentence:
                     w = Word(word)
-                    if w.is_subject():
-                        subj = w
-                    elif w.is_relation():
-                        verb = w
-                    elif w.is_direct_object():
-                        direct_obj= w
-                    elif w.is_indirect_object():
-                        indirect_obj = w
+                    words[w.id()] = w
+
+        for id, w in words.items():
+            parent = words.get(w.parent_id())
+            # print id
+            # print 'parent : ' + str(w.parent_id())
+            # if parent:
+            #     print str(parent.id())
+            w.set_parent(parent)
+
+        for id, w in words.items():
+            if w.is_subject():
+                subj = w
+            elif w.is_relation():
+                verb = w
+            elif w.is_direct_object():
+                direct_obj = w
+            elif w.is_indirect_object():
+                indirect_obj = w
+
 
         db = Db()
 
+        ans = []
+        if verb and not verb.is_unknown_word():
+            from_entity_name = subj.get_content() if subj and not subj.is_unknown_word() else None
+            to_entity_name = direct_obj.get_content() if direct_obj and not direct_obj.is_unknown_word() else None
+            IO = indirect_obj.get_content() if indirect_obj and not indirect_obj.is_unknown_word() else None
 
-        if subj.is_unknown_word():
-            if verb:
-                relations = db.find_relation(verb.get_content())
-                for r in relations:
-                    yield
+            relations = db.find_relations(verb.get_content(), from_entity_name=from_entity_name, to_entity_name=to_entity_name,
+                                          IO=IO)
 
-    def is_unknown_word(self, word):
-        return word in ['谁','什么','怎么','怎么样','为什么']
+            for relation in relations:
+                if subj and subj.is_unknown_word():
+                    ans.append(db.find_from_entity(relation))
+                elif direct_obj and direct_obj.is_unknown_word():
+                    ans.append(db.find_to_entity(relation))
+                elif indirect_obj and indirect_obj.is_unknown_word():
+                    ans.append(relation["IO"])
+
+        return ans
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
