0a145fc796887903778effdb3d46a9622c5a43a4
Junchao Wu <junchaow@uber.com>
2016-11-20 17:51:21 -0800

    add more code

--- knowledge/ltp.py
+++ knowledge/ltp.py
@@ -1,10 +1,19 @@
 # coding=utf8
+from __future__ import absolute_import
 
 import urllib
 import argparse
 import json
 
 API_KEY = 'K8L7C0w9yxVGAK3Q7q3euHtsbtfzOgMsya4Fyb8x'
+
+DependencyParsing = {
+    'sbv', 'forward',  # 主谓关系
+    'vob', 'forward',  # 动宾关系
+    'iob', 'forward',  # 间宾关系
+    'fob', 'backward'  # 前置宾语
+}
+
 
 class Ltp(object):
 
@@ -21,13 +30,9 @@ class Ltp(object):
         url = 'http://api.ltp-cloud.com/analysis/?' + urllib.urlencode(params)
         return url
 
+
 def handle_request(response):
     body = json.loads(response)
-    for paragraph in body:
-        for sentence in paragraph:
-            for word in sentence:
-                print word
-
     return body
 
 
